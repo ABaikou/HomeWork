6a7b1c14e0d5ca992b91d0107edad6d152221cc5
ABaikou <62279262+ABaikou@users.noreply.github.com>
2020-03-31 20:50:16 +0300

    Add HomeWork_1.sln

---
+++ HomeWork_1/DayOfTheWeek/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DayOfTheWeek
+{
+    class Program
+    {
+        static void OutputDayName(string dayNum)
+        {
+            switch (dayNum)
+            {
+                case "1":
+                    Console.WriteLine("Monday");
+                    break;
+                case "2":
+                    Console.WriteLine("Tuesday");
+                    break;
+                case "3":
+                    Console.WriteLine("Wednesday");
+                    break;
+                case "4":
+                    Console.WriteLine("Thursday");
+                    break;
+                case "5":
+                    Console.WriteLine("Friday");
+                    break;
+                case "6":
+                    Console.WriteLine("Saturday");
+                    break;
+                case "7":
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input");
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day number (1...7): ");
+            string dayNumber = Console.ReadLine();
+            OutputDayName(dayNumber);
+        }
+    }
+}
---
+++ HomeWork_1/MultiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void OutputMultTable(int x)
+        {
+            Console.WriteLine($"Multiplication table for: {x}");
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{x} x {i} = {x * i}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number (integer): ");
+            int num;
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out num))
+            {
+                OutputMultTable(num);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+            }
+
+        }
+    }
+}
